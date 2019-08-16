56ae11d3c18f5e8d1d6dd49389919b6c9da741fa
Tezan <tezansahu@gmail.com>
2019-08-16 09:35:07 +0530

    Q1(b) done

---
+++ assignment-1-transformations/1/codes/myBilinearInterpolation.m
@@ -0,0 +1,35 @@
+function [newImg] = bilinearInterpolation(im)
+
+    in_rows = size(im)(1);
+    in_cols = size(im)(2);
+    out_rows = 3*in_rows - 2;
+    out_cols = 2*in_cols - 1;
+      
+    r_scale = in_rows / out_rows;
+    c_scale = in_cols / out_cols;
+
+    [cf, rf] = meshgrid(1 : out_cols, 1 : out_rows);
+    rf = rf * r_scale;
+    cf = cf * c_scale;
+
+    r = floor(rf);
+    c = floor(cf);
+
+    r(r < 1) = 1;
+    c(c < 1) = 1;
+    r(r > in_rows - 1) = in_rows - 1;
+    c(c > in_cols - 1) = in_cols - 1;
+
+    del_R = rf - r;
+    del_C = cf - c;
+
+    in1_ind = sub2ind([in_rows, in_cols], r, c);
+    in2_ind = sub2ind([in_rows, in_cols], r+1,c);
+    in3_ind = sub2ind([in_rows, in_cols], r, c+1);
+    in4_ind = sub2ind([in_rows, in_cols], r+1, c+1);       
+
+    im = double(im);
+    tmp = im(in1_ind).*(1 - del_R).*(1 - del_C) + im(in2_ind).*(del_R).*(1 - del_C) + im(in3_ind).*(1 - del_R).*(del_C) + im(in4_ind).*(del_R).*(del_C);
+    newImg = cast(tmp, class(im));
+
+endfunction
--- assignment-1-transformations/1/codes/myMainScript.m
+++ assignment-1-transformations/1/codes/myMainScript.m
@@ -1,9 +1,9 @@
 %% MyMainScript
 
-tic;
 %% Your code here
 
 %% Part (a): Image Shrinking
+tic;
 img_a = imread("../data/circles_concentric.png");
 showImage(img_a, "Original Image");
 %%pause(2);
@@ -14,5 +14,15 @@ for i = 1:1:size(d)(2)
   showImage(smallImage, heading);
   %%pause(2);
 endfor;
- 
+toc;
+
+
+%% Part (b): Image Enlarging by Bilear Interpolation
+tic;
+img_b = imread("../data/barbaraSmall.png");
+showImage(img_b, "Original Image");
+%%pause(2);
+newImg = myBilinearInterpolation(img_b);
+showImage(newImg, "Image Enlarging using Bilear Interpolation");
+%%pause(2);
 toc;
