8139b74b1fee924456a7846b350293ea74fdddfc
Tezan <tezansahu@gmail.com>
2019-08-23 10:49:35 +0530

    modified foreground mask to consider any threshold

--- assignment-1-transformations/2/codes/myForegroundMask.m
+++ assignment-1-transformations/2/codes/myForegroundMask.m
@@ -1,13 +1,13 @@
-function [mask, masked_img] = myForegroundMask(img)
-%MYFOREGROUNDMASK Binary mask for foreground region of image
-%   Detailed explanation goes here
-% imshow(img)
-chan = size(img, 3);
-mask = double(img);
-masked_img = double(img);
-for i=1:1:chan
-    mask(:,:,i) = (img(:,:,i)>10)*255;
-    % immultiply- multiplies each element in array X by the corresponding element in array Y
-    masked_img(:,:,i) = immultiply((mask(:,:,i)~=0),img(:,:,i));
-imshow(masked_img);
+function [mask, masked_img] = myForegroundMask(img, threshold)
+% Assuming img has only 1 channel (since it is used only for image (7))
+% chan = size(img, 3);
+orig_img = img;
+img(img < threshold) = 0;
+img(img >= threshold) = 255;
+mask = logical(medfilt2(medfilt2(img)));
+
+% immultiply- multiplies each element in array X by the corresponding element in array Y
+% masked_img(:,:,i) = immultiply((mask(:,:,i)~=0),img(:,:,i));
+masked_img = mask .* orig_img;
+% imshow(masked_img);
 end
