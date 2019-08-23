30813cc9c35c624eea8404b915d71c6ee0997b9b
Tezan <tezansahu@gmail.com>
2019-08-23 10:14:08 +0530

    mask feature added to HE & LCS

--- assignment-1-transformations/2/codes/myLinearContrastStretching.m
+++ assignment-1-transformations/2/codes/myLinearContrastStretching.m
@@ -1,9 +1,11 @@
-function [newImg] = myLinearContrastStretching(img)
+function [newImg] = myLinearContrastStretching(img, mask)
+  %% For all images except (7), the mask is an all ones matrix of the same size as img
   chan = size(img,3);
   newImg = img;
   for i=1:1:chan 
-    minI = min(min(img(:,:,i)));
-    maxI = max(max(img(:,:,i)));
-    newImg(:,:,i) = (img(:,:,i) - minI) * ((255 - 0) / (maxI - minI));
+    img_chan = img(:,:,i);
+    minI = min(min(img_chan(mask ==1)));
+    maxI = max(max(img_chan(mask ==1)));
+    newImg(:,:,i) = mask .* (img_chan - minI) * ((255 - 0) / (maxI - minI));
   end
 endfunction;
