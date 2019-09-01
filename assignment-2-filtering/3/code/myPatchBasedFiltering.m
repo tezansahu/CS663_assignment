c386d7e3c2ee2e91137d0fc2beb794c521fc977c
Tezan <tezansahu@gmail.com>
2019-09-02 01:38:04 +0530

    Asgn2 Q3 done

--- assignment-2-filtering/3/code/myPatchBasedFiltering.m
+++ assignment-2-filtering/3/code/myPatchBasedFiltering.m
@@ -1,47 +1,34 @@
 function filtered_img = myPatchBasedFiltering(img, sigma_i)
     filtered_img = double(zeros(size(img,1), size(img,2)));
-    % disp(filtered_img);
     window = 25;
     patch_size = 9; 
+    
+    pad_size = floor(window/2) + floor(patch_size/2);
+    
+    % Use Neumann Boundary Condition for padding around the image
+    img_padded = padarray(img, [pad_size, pad_size], 'replicate');
     wait = waitbar(0, "Patch-Based Filtering in Progress");
     
     % define the gaussian mask to make patches isotropic
-    sigma = 0.5;
+    sigma = 4/3;
     [x,y] = meshgrid(-floor(patch_size/2):floor(patch_size/2), -floor(patch_size/2):floor(patch_size/2));
     G_p = exp(-(x.^2 + y.^2)/(2*sigma^2));
     
-    % define the spatial gaussian mask to be used on the window
-    sigma_s = 0.5;
-    [x,y] = meshgrid(-floor(window/2):floor(window/2), -floor(window/2):floor(window/2));
-    G_s = exp(-(x.^2 + y.^2)/(2*sigma_s^2));
-    
     % Pass over the image
-    for i = 1:size(img,1)
+    for i = 1+pad_size:size(img,1)+pad_size
         % disp(i);
         % max & min used to account for edge pixels
-        % i_min = max(i - floor(window/2), 1);    
-        % i_max = min(i + floor(window/2), size(img,1));
+        i_min = max(i - floor(window/2), 1);    
+        i_max = min(i + floor(window/2), size(img_padded,1));
         
-        % select row indices to form window around the chosen pixel
-        % (implementing wraparound)
-        rows = mod((i - floor(window/2) - 1):(i + floor(window/2) - 1), size(img,1)) + 1;
-        
-        for j = 1:size(img,2)
+        for j = 1+pad_size:size(img,2)+pad_size
 
             % max & min used to account for edge pixels
-            % j_min = max(j - floor(window/2), 1);
-            % j_max = min(j + floor(window/2), size(img,2));
+            j_min = max(j - floor(window/2), 1);
+            j_max = min(j + floor(window/2), size(img_padded,2));
             % disp(strcat("Window: i=", num2str(i), ", j=", num2str(j)))
             
-            % select column indices to form window around the chosen pixel
-            % (implementing wraparound)
-            cols = mod((j - floor(window/2) - 1):(j + floor(window/2) - 1), size(img,2)) + 1;
-            % disp(rows);
-            % disp(cols);
-            % disp(size(img(rows, cols)));
-            % imshow(img(rows, cols));
-            
-            filtered_img(i,j) = getPatchBasedIntensity(img(rows, cols), G_p, G_s, sigma_i, patch_size);
+            filtered_img(i - pad_size,j - pad_size) = getPatchBasedIntensity(img_padded, [i_min, i_max, j_min, j_max], G_p, sigma_i, patch_size);
             
         end
         waitbar(i/size(img,1));
@@ -50,49 +37,37 @@ function filtered_img = myPatchBasedFiltering(img, sigma_i)
 end
 
 
-function pixel_intensity = getPatchBasedIntensity(window, G_p, G_s, sigma_i, patch_size)
-    % curr_patch_i_min = max(i - floor(patch_size/2), 1);
-    % curr_patch_i_max = min(i + floor(patch_size/2), window_lims(2));
-    % curr_patch_j_min = max(j - floor(patch_size/2), 1);
-    % curr_patch_j_max = min(j + floor(patch_size/2), window_lims(4));
-    % curr_patch = img(curr_patch_i_min:curr_patch_i_max, curr_patch_j_min:curr_patch_j_max);
-    % curr_patch_vector = reshape(curr_patch, [1, (curr_patch_i_max - curr_patch_i_min) * (curr_patch_j_max - curr_patch_j_min)]);
+function pixel_intensity = getPatchBasedIntensity(img_padded, window_limits, G_p, sigma_i, patch_size)
+    center_indices = [(window_limits(1)+window_limits(2))/2, (window_limits(3)+window_limits(4))/2];
     
-    window_size = size(window,1);
-    weights = zeros([window_size, window_size]);
+    curr_patch_i_min = max(center_indices(1) - floor(patch_size/2), 1);
+    curr_patch_i_max = min(center_indices(1) + floor(patch_size/2), size(img_padded,1));
+    curr_patch_j_min = max(center_indices(2) - floor(patch_size/2), 1);
+    curr_patch_j_max = min(center_indices(2) + floor(patch_size/2), size(img_padded,2));
+    curr_patch = img_padded(curr_patch_i_min:curr_patch_i_max, curr_patch_j_min:curr_patch_j_max);
+    curr_patch = curr_patch .* G_p;
+    
+    weights = zeros([window_limits(2) - window_limits(1) + 1, window_limits(4) - window_limits(3) + 1]);
     % disp(strcat("Weights size:", num2str(size(weights,1)), " * ", num2str(size(weights,2))));
     
-    % Get patch around center pixel & convert it into vector
-    curr_patch_rows = mod((ceil(window_size/2) - floor(patch_size/2) - 1): (ceil(window_size/2) + floor(patch_size/2) - 1), window_size);
-    curr_patch_cols = mod((ceil(window_size/2) - floor(patch_size/2) - 1): (ceil(window_size/2) + floor(patch_size/2) - 1), window_size);
-    curr_patch = window(curr_patch_rows, curr_patch_cols);
-    % Use the mask to make patches isotropic
-    curr_patch_vec = reshape(curr_patch, [1, patch_size^2]);
-    
-    for p_i = 1:1:window_size
-        % patch_i_min = max(p_i - floor(patch_size/2), 1);    
-        % patch_i_max = min(p_i + floor(patch_size/2), window_lims(2));
+    for p_i = window_limits(1):1:window_limits(2)
+        patch_i_min = max(p_i - floor(patch_size/2), 1);    
+        patch_i_max = min(p_i + floor(patch_size/2), size(img_padded,1));
         
-        % select row indices to form patch around the chosen pixel in the
-        % window (implementing wraparound)
-        patch_rows = mod((p_i - floor(patch_size/2) - 1):(p_i + floor(patch_size/2) - 1), window_size) + 1;
-        for p_j = 1:1:window_size
-            % select column indices to form patch around the chosen pixel in the
-            % window (implementing wraparound)
-            patch_cols = mod((p_i - floor(patch_size/2) - 1):(p_i + floor(patch_size/2) - 1), window_size) + 1; 
-            patch = window(patch_rows, patch_cols);
-            % Use the mask to make patch isotropic
-            patch_vec = reshape(patch, [1, patch_size^2]);
+        for p_j = window_limits(3):1:window_limits(4)
+            patch_j_min = max(p_j - floor(patch_size/2), 1);    
+            patch_j_max = min(p_j + floor(patch_size/2), size(img_padded,2));
+            
+            patch = img_padded(patch_i_min:patch_i_max, patch_j_min:patch_j_max);
+            patch = patch .* G_p; 
             
             % Compute weight of a patch for the center pixel
-            weights(p_i,p_j) = G_s(p_i, p_j) * exp(-sum((curr_patch_vec - patch_vec) .^ 2)/sigma_i^2);
+            weights(p_i - window_limits(1) + 1, p_j - window_limits(3) + 1) = exp(-sum(sum((curr_patch - patch) .^ 2))/sigma_i^2);
             % disp(strcat("Patch: i=", num2str(p_i), ", j=", num2str(p_j)));
         end
     end
-    % disp(size(weights));
-    % disp(size(window)); 
-    
+  
     % Compute actual intensity of center pixel of the window using weights
     % of patches
-    pixel_intensity = sum(sum(weights .* window))/sum(sum(weights));
+    pixel_intensity = sum(sum(weights .* img_padded(window_limits(1):window_limits(2), window_limits(3):window_limits(4))))/sum(sum(weights));
 end
