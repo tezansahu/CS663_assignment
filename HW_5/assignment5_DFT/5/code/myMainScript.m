a673f73d8d8fc8e16f9be3a44c61819478abbaa5
ahas <sahasiddharth611@gmail.com>
2019-10-21 13:42:28 +0530

    Added codes for Q3 and Q4; Added cues to .m file of Q6

--- HW_5/assignment5_DFT/5/code/myMainScript.m
+++ HW_5/assignment5_DFT/5/code/myMainScript.m
@@ -1,6 +1,12 @@
 %% MyMainScript
 
 tic;
-%% Your code here
+%% Solution
+cd ../data;
+im = imread("barbara256-part.png");
+
+% Adding zero mean Gaussian noise of sd=20
+im1 = im + randn(size(im))*20;
+
 
 toc;
--- HW_5/assignment5_DFT/6/code/myMainScript.m
+++ HW_5/assignment5_DFT/6/code/myMainScript.m
@@ -3,4 +3,33 @@
 tic;
 %% Your code here
 
+clear;
+close all;
+A = zeros(512);
+B = A;
+A(50:100,50:100) = 1;
+B(120:170,20:70) = 1;
+
+figure(1); imshow(A); figure(2); imshow(B);
+
+FA = fftshift(fft2(A));
+lFA = log(abs(FA)+1); figure(3); imshow(lFA,[min(lFA(:)) max(lFA(:))]); colormap('jet'); colorbar;
+FB = fftshift(fft2(B));
+lFB = log(abs(FB)+1); figure(4); imshow(lFB,[min(lFB(:)) max(lFB(:))]); colormap('jet'); colorbar;
+pause(1);
+
+FC = ((FA)'.*FB)./(abs(FA.*FB));
+C = ifft2((FC));
+figure; imshow(C/max(C(:)));
+impixelinfo;
+
+
+% In probability theory and statistics, a collection of random variables is independent and identically 
+% distributed if each random variable has the same probability distribution as the others and all are mutually
+% independent. This property is usually abbreviated as i.i.d. or iid or IID
+
+% How to add zero mean Gaussian noise of sd=20
+% im1 = im + randn(size(im))*20;
+
+
 toc;
