aabd0d05896277887dd8a70b7c15c6f0820a58fc
Tezan <tezansahu@gmail.com>
2019-10-29 08:19:01 +0530

    added basic code for HW5 Q6 for checking

--- HW_5/assignment5_DFT/6/code/myMainScript.m
+++ HW_5/assignment5_DFT/6/code/myMainScript.m
@@ -2,5 +2,21 @@
 
 tic;
 %% Your code here
+I = zeros(300);
+I(50:100, 50:120) = 255;
+J = zeros(300);
+J(120:170, 20:90) = 255;
+figure(1);
+subplot(121); imshow(I); title("Image I"); subplot(122); imshow(J); title("Image J");
+
+f1 = fftshift(fft2(I)); f2 = fftshift(fft2(J));
+f = (f1 .* conj(f2))./(abs(f1 .* f2));
+lf = log(abs(f) + 1);
+figure(2); imshow(lf, [min(lf(:)), max(lf(:))]); colormap jet; colorbar; impixelinfo; title("log(|F(u,v)| + 1) [This should be constant]")
+g = real(ifft2(f));
+figure(3);
+imshow(g/max(g(:))); title("Shift neede for Restoration")
+impixelinfo;
+
 
 toc;
