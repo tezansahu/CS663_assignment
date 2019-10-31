5ab90c30d1f7a5ffd08866f8542c69046257f44b
Tezan <tezansahu@gmail.com>
2019-10-31 17:09:55 +0530

    Completed Q6 coding

--- HW_5/assignment5_DFT/6/code/myMainScript.m
+++ HW_5/assignment5_DFT/6/code/myMainScript.m
@@ -1,7 +1,10 @@
 %% MyMainScript
+clear;
+close all;
 
 tic;
-%% Your code here
+%% Applying the algorithm for Original Images
+
 I = zeros(300);
 I(50:100, 50:120) = 255;
 J = zeros(300);
@@ -12,40 +15,34 @@ subplot(121); imshow(I); title("Image I"); subplot(122); imshow(J); title("Image
 f1 = fftshift(fft2(I)); f2 = fftshift(fft2(J));
 f = (f1 .* conj(f2))./(abs(f1 .* f2));
 lf = log(abs(f) + 1);
-figure(2); imshow(lf, [min(lf(:)), max(lf(:))]); colormap jet; colorbar; impixelinfo; title("log(|F(u,v)| + 1) [This should be constant]")
-g = real(ifft2(f));
+
+figure(2); 
+imshow(lf, [min(lf(:)), max(lf(:))]); colormap jet; colorbar; impixelinfo; title("log(|F(u,v)| + 1) [This should be constant]")
+g = ifft2(f);
+
 figure(3);
-imshow(g/max(g(:))); title("Shift neede for Restoration")
+imshow(g/max(g(:))); title("Shift needed on Image J for Restoration")
 impixelinfo;
 
+%% Applying the same for the Noisy Images
 
-clear;
-close all;
-A = zeros(512);
-B = A;
-A(50:100,50:100) = 1;
-B(120:170,20:70) = 1;
+I_noisy = I + rand(size(I)) * 20;
+J_noisy = J + rand(size(I)) * 20;
+figure(4);
+subplot(121); imshow(I_noisy, [min(I_noisy(:)), max(I_noisy(:))]); title("Image I [Noisy]"); subplot(122); imshow(J_noisy, [min(J_noisy(:)), max(J_noisy(:))]); title("Image J [Noisy]");
+impixelinfo
 
-figure(1); imshow(A); figure(2); imshow(B);
+f1_noisy = fftshift(fft2(I_noisy)); f2_noisy = fftshift(fft2(J_noisy));
+f_noisy = (f1_noisy .* conj(f2_noisy))./(abs(f1_noisy .* f2_noisy));
+lf_noisy = log(abs(f_noisy) + 1);
 
-FA = fftshift(fft2(A));
-lFA = log(abs(FA)+1); figure(3); imshow(lFA,[min(lFA(:)) max(lFA(:))]); colormap('jet'); colorbar;
-FB = fftshift(fft2(B));
-lFB = log(abs(FB)+1); figure(4); imshow(lFB,[min(lFB(:)) max(lFB(:))]); colormap('jet'); colorbar;
-pause(1);
+figure(5); 
+imshow(lf_noisy, [min(lf_noisy(:)), max(lf_noisy(:))]); colormap jet; colorbar; impixelinfo; title("log(|F(u,v)| + 1) [This should be constant]")
 
-FC = ((FA)'.*FB)./(abs(FA.*FB));
-C = ifft2((FC));
-figure; imshow(C/max(C(:)));
+g_noisy = ifft2(f_noisy);
+figure(6);
+imshow(g_noisy/max(g_noisy(:))); title("Shift needed on Image J (Noisy) for Restoration")
 impixelinfo;
-
-
-% In probability theory and statistics, a collection of random variables is independent and identically 
-% distributed if each random variable has the same probability distribution as the others and all are mutually
-% independent. This property is usually abbreviated as i.i.d. or iid or IID
-
-% How to add zero mean Gaussian noise of sd=20
-% im1 = im + randn(size(im))*20;
 
 
 toc;
